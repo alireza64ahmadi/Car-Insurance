90f27eaff847f1b9d7e359404abf1d9eeade0e00
alireza <121108490+alireza64ahmadi@users.noreply.github.com>
2023-10-14 19:41:39 +0330

    add: fix number function and change the name
    :fixnumber function and change the name

--- js/app.js
+++ js/app.js
@@ -94,7 +94,6 @@ function calculateLevel(level , price){
     */
 
     if (level == 'basic'){
-        // price = price + (price * 0.30) (bara mehrdad)
         price = price * 1.3
     }else{
         price = price * 1.5
@@ -124,7 +123,7 @@ function displayMsg(msg) {
 // Show Years
 function displayYears() {
     // Convert to number
- 
+    fixNumbers(str)
     // get now years
     let curentYear = new Date().toLocaleDateString('fa-IR')
 
@@ -158,4 +157,16 @@ function displayYears() {
         // append option to the selectYear
         selectYear.appendChild(optionTag)
     }
+}
+function fixNumbers(str){
+    let
+    persianNumbers = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g],
+    arabicNumbers = [/٠/g, /١/g, /٢/g, /٣/g, /٤/g, /٥/g, /٦/g, /٧/g, /٨/g, /٩/g]
+
+        if (typeof str === 'string') {
+            for (var i = 0; i < 10; i++) {
+                str = str.replace(persianNumbers[i], i).replace(arabicNumbers[i], i);
+            }
+        }
+        return parseInt(str);
 }
